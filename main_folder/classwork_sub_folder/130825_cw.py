282d46b0867ce8b6a5f17c27dfe2ebceae82ff33
Volodymyr Potereiko(AIPython52) <poter_vi94@student.itstep.org>
2025-08-13 20:23:17 +0300

    Git log перед цим комітом

--- main_folder/classwork_sub_folder/130825_cw.py
+++ main_folder/classwork_sub_folder/130825_cw.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+# $ git log
+# commit ca9f6ab886bf650d860f9188cd83b6f1547d78b1 (HEAD -> master)
+# Author: Volodymyr Potereiko(AIPython52) <poter_vi94@student.itstep.org>
+# Date:   Wed Aug 13 20:14:31 2025 +0300
+#
+#     Git start commit
+#
+# user@CEPBEPOKK MINGW64 ~/OneDrive/__AIPYTHON52/ALL_TASKS/08_2025/130825/git130825_1 (master)
